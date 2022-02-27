c063c446a97416c55ad6fb11ded993ebae6fbc73
Šimon Koláčka <kolackas.04@spst.cz>
2022-02-27 17:38:22 +0100

    visual, timer, vymyšlení kostry a funkce celé aplikace

--- bruteforce_shit/Form1.Designer.cs
+++ bruteforce_shit/Form1.Designer.cs
@@ -29,23 +29,123 @@ namespace bruteforce_shit
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Bruteforce));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBoxVstup = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelYourPswd = new System.Windows.Forms.Label();
+            this.labelMadeBy = new System.Windows.Forms.Label();
+            this.labelEstimatedTime = new System.Windows.Forms.Label();
+            this.timerEstimatedTime = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.progressBar1.Location = new System.Drawing.Point(108, 254);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(327, 10);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // textBoxVstup
+            // 
+            this.textBoxVstup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxVstup.Font = new System.Drawing.Font("Ubuntu Mono", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxVstup.ForeColor = System.Drawing.Color.White;
+            this.textBoxVstup.Location = new System.Drawing.Point(192, 31);
+            this.textBoxVstup.Name = "textBoxVstup";
+            this.textBoxVstup.Size = new System.Drawing.Size(161, 24);
+            this.textBoxVstup.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonStart.Font = new System.Drawing.Font("Ubuntu Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStart.ForeColor = System.Drawing.Color.White;
+            this.buttonStart.Location = new System.Drawing.Point(207, 211);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(125, 37);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "BruteForce";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelYourPswd
+            // 
+            this.labelYourPswd.AutoSize = true;
+            this.labelYourPswd.BackColor = System.Drawing.Color.Transparent;
+            this.labelYourPswd.Font = new System.Drawing.Font("Ubuntu Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelYourPswd.ForeColor = System.Drawing.Color.White;
+            this.labelYourPswd.Location = new System.Drawing.Point(27, 31);
+            this.labelYourPswd.Name = "labelYourPswd";
+            this.labelYourPswd.Size = new System.Drawing.Size(135, 20);
+            this.labelYourPswd.TabIndex = 3;
+            this.labelYourPswd.Text = "your password:";
+            // 
+            // labelMadeBy
+            // 
+            this.labelMadeBy.AutoSize = true;
+            this.labelMadeBy.BackColor = System.Drawing.Color.Transparent;
+            this.labelMadeBy.Font = new System.Drawing.Font("Ubuntu Mono", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelMadeBy.ForeColor = System.Drawing.Color.White;
+            this.labelMadeBy.Location = new System.Drawing.Point(419, 9);
+            this.labelMadeBy.Name = "labelMadeBy";
+            this.labelMadeBy.Size = new System.Drawing.Size(109, 60);
+            this.labelMadeBy.TabIndex = 4;
+            this.labelMadeBy.Text = "//made by \r\n//kolackasimon\r\n//houzarlukas\r\n//kristofcakrobin";
+            // 
+            // labelEstimatedTime
+            // 
+            this.labelEstimatedTime.AutoSize = true;
+            this.labelEstimatedTime.BackColor = System.Drawing.Color.Transparent;
+            this.labelEstimatedTime.Font = new System.Drawing.Font("Ubuntu Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelEstimatedTime.ForeColor = System.Drawing.Color.White;
+            this.labelEstimatedTime.Location = new System.Drawing.Point(28, 143);
+            this.labelEstimatedTime.Name = "labelEstimatedTime";
+            this.labelEstimatedTime.Size = new System.Drawing.Size(113, 16);
+            this.labelEstimatedTime.TabIndex = 5;
+            this.labelEstimatedTime.Text = "estimated time:";
+            // 
+            // timerEstimatedTime
+            // 
+            this.timerEstimatedTime.Tick += new System.EventHandler(this.timerEstimatedTime_Tick);
             // 
             // Bruteforce
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(614, 380);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(540, 271);
+            this.Controls.Add(this.labelEstimatedTime);
+            this.Controls.Add(this.labelMadeBy);
+            this.Controls.Add(this.labelYourPswd);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxVstup);
+            this.Controls.Add(this.progressBar1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Bruteforce";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BruteForce | Application";
+            this.Load += new System.EventHandler(this.Bruteforce_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBoxVstup;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelYourPswd;
+        private System.Windows.Forms.Label labelMadeBy;
+        private System.Windows.Forms.Label labelEstimatedTime;
+        private System.Windows.Forms.Timer timerEstimatedTime;
     }
 }
 
--- bruteforce_shit/Form1.cs
+++ bruteforce_shit/Form1.cs
@@ -16,5 +16,39 @@ namespace bruteforce_shit
         {
             InitializeComponent();
         }
+
+        int estimatedTime = 0;
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            //nevim jak ale 
+            //pokud je heslo hodně slabý <0-25%>- buttonStart.BackColor = Color.Red;
+            //slabý <26-50>- buttonStart.BackColor = Color.Orange;
+            //slabý <51-75>- buttonStart.BackColor = Color.Blue;
+            //najs <75-100>- buttonStart.BackColor = Color.Green;
+            progressBar1.Visible = true;
+            MessageBox.Show("//we have found the password", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+  
+            timerEstimatedTime = new Timer();//ESTIMATED TIME
+            timerEstimatedTime.Tick += new EventHandler(timerEstimatedTime_Tick);
+            timerEstimatedTime.Interval = 1000; // 1 s
+            timerEstimatedTime.Start();
+            labelEstimatedTime.Text = "estimated time: " + estimatedTime.ToString();
+        }
+
+        private void Bruteforce_Load(object sender, EventArgs e)
+        {
+            progressBar1.Visible = false;
+            buttonStart.TabStop = false;
+            buttonStart.FlatStyle = FlatStyle.Flat;
+            buttonStart.FlatAppearance.BorderSize = 0;
+        }
+
+        private void timerEstimatedTime_Tick(object sender, EventArgs e)
+        {
+            estimatedTime++;
+            if (estimatedTime==10) { timerEstimatedTime.Stop(); } //NASTAVIT KONEC TIMERU!!!
+            labelEstimatedTime.Text = "estimated time: "+estimatedTime.ToString();
+        }
     }
 }
